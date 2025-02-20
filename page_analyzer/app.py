d1d1e4487046bfeddb24a489495c15d19006054b
Victoria Treskova <vikatresk@yandex.ru>
2025-02-20 19:30:34 +0500

    blank line fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -99,6 +99,7 @@ def check_url(id):
 def page_not_found(e):
     return render_template('errors/404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("errors/500.html"), 500
